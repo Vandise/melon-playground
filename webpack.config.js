b47bb8b76f5e85d5ecb38a5c9c75b2f5c03b5d01
Benjamin J Anderson <banderson@mashable.com>
2017-02-19 11:35:45 -0600

    Initial Setup

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [
+    "webpack-dev-server/client?http://0.0.0.0:9090",
+    'webpack/hot/only-dev-server',
+    './lib/melonJS.js',
+    './lib/plugins/debugPanel.js',
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: 'bundle.js'
+  },
+  resolveLoader: {
+    modulesDirectories: ['node_modules']
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './src/static/', to: path.join(__dirname, "public")},
+    ]),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.IgnorePlugin(/vertx/)
+  ],
+  debug: true,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/,    loader: 'babel', include: path.join(__dirname, 'src'), exclude: '/node_modules/' },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+    ]
+  },
+  node: {
+    fs: "empty"
+  }
+};
