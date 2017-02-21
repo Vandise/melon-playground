8d39ddfdfc90283f44b414d6c0cf3d57debc7d03
Benjamin J Anderson <banderson@mashable.com>
2017-02-21 11:07:55 -0600

    Initial changes

--- src/client/screens/play.js
+++ src/client/screens/play.js
@@ -1,18 +1,13 @@
 import game from '../game';
 
 class PlayScreen extends me.ScreenObject {
-  /**
-  *  action to perform on state change
-  */
+
   onResetEvent() {
     me.game.world.addChild(new me.ColorLayer("background", "#ff0000", 0), 0);        
   }
-  
-  /**
-  *  action to perform when leaving this screen (state change)
-  */
+
   onDestroyEvent() {
-    
+
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -9,40 +9,40 @@ class Bootstrap {
       alert("Your browser does not support HTML5 canvas.");
       return;
     }
-    
+
     // add "#debug" to the URL to enable the debug Panel
     if (document.location.hash === "#debug") {
       window.onReady(function () {
         me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
       });
     }
-    
+
     // Initialize the audio.
     me.audio.init("mp3,ogg");
-    
+
     // Set a callback to run when loading is complete.
     me.loader.onload = this.loaded.bind(this);
-    
+
     // Load the resources.
     me.loader.preload({});
-    
+
     // Initialize melonJS and display a loading screen.
     me.state.change(me.state.LOADING);
   }
 
   loaded() {
     me.state.set(me.state.PLAY, new PlayScreen());
-    
+
     // add our player entity in the entity pool
     // me.pool.register("mainPlayer", PlayerEntity);
-    
+
     // Start the game.
-    me.state.change(me.state.PLAY);     
+    me.state.change(me.state.PLAY);
   }
 
   static boot() {
     var bootstrap = new Bootstrap();
-    
+
     // Mobile browser hacks
     if (me.device.isMobile && !navigator.isCocoonJS) {
       // Prevent the webview from moving on a swipe
@@ -51,18 +51,18 @@ class Bootstrap {
       window.scroll(0, 0);
       return false;
       }, false);
-      
+
       // Scroll away mobile GUI
       (function () {
         window.scrollTo(0, 1);
         me.video.onresize(null);
       }).defer();
-      
+
       me.event.subscribe(me.event.WINDOW_ONRESIZE, function (e) {
         window.scrollTo(0, 1);
       });
     }
-    
+
     return bootstrap;
   }
 }
