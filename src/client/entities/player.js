35cfad9b60b5847e28ce43432c0e533cdd853ce5
Benjamin J. Anderson <andeb2804@gmail.com>
2017-03-03 20:34:43 -0600

    Add frame padding for smaller animations

--- src/client/entities/player.js
+++ src/client/entities/player.js
@@ -1,12 +1,13 @@
 
 const FRAME_PADDING = 9;
+const ANIMATION_SPEED = 50;
 const DIRECTIONS = ['east', 'north', 'northeast', 'northwest', 'south', 'southeast', 'southwest', 'west'];
 const ANIMATIONS = {
   stand:  { frames: 8, framesPerDirection: false, speed: 0  },
-  walk:   { frames: 9, framesPerDirection: true,  speed: 250 },
-  attack: { frames: 9, framesPerDirection: true,  speed: 250 },
-  death:  { frames: 9, framesPerDirection: true,  speed: 250 },
-  hit:    { frames: 8, framesPerDirection: true,  speed: 250 }
+  walk:   { frames: 9, framesPerDirection: true,  speed: ANIMATION_SPEED },
+  attack: { frames: 9, framesPerDirection: true,  speed: 100 },
+  death:  { frames: 9, framesPerDirection: true,  speed: 100 },
+  hit:    { frames: 8, framesPerDirection: true,  speed: ANIMATION_SPEED }
 };
 
 
@@ -40,6 +41,9 @@ export default class Player extends me.Entity {
             currentFrame += 1;
             return frame;
           });
+          if (frames.length < FRAME_PADDING) {
+            currentFrame += FRAME_PADDING - (frames.length);
+          }
           console.log(animation, direction, frames, currentFrame);
           this.renderable.addAnimation(`${animation}_${direction}`, frames);
         });
@@ -53,7 +57,7 @@ export default class Player extends me.Entity {
           currentFrame += FRAME_PADDING - (frames.length);
         }
         console.log(animation, frames, currentFrame);
-        this.renderable.addAnimation(animation, frames);
+        this.renderable.addAnimation(animation, frames, config.speed);
       }
     });
 
